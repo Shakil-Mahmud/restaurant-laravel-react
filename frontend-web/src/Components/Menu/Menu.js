4801dd2d2cb91efdf74e1fb1a7da292c0e3cb24d
XI-SKL <khchow@xandi.co.jp>
2022-11-09 17:47:09 +0600

    create category table

---
+++ frontend-web/src/Components/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <>
+    <div className="flex">
+    
+    </div>
+    </>
+  )
+}
+
+export default Menu
---
+++ frontend-web/src/Components/components.js
@@ -0,0 +1,3 @@
+export { default as HeroSection } from "./HeroSection/HeroSection";
+export { default as Menu } from "./Menu/Menu";
+export { default as MenuItem } from "./Menu/MenuItem";
